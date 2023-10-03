a3530a1e3574dc2d97d933ec7c5431a3f659b22d
iamhusam <husamahmud@gmail.com>
2023-10-03 20:04:31 +0300

    Add 15

--- 15-3Sum.py
+++ 15-3Sum.py
@@ -24,24 +24,3 @@ class Solution(object):
                         l += 1
 
         return res
-
-
-if __name__ == '__main__':
-    nums = [0, 0, 0]
-    res = []
-    l = 0
-    m = l + 1
-    r = len(nums) - 1
-
-    nums.sort()
-    while m < r:
-        if nums[r] + nums[m] + nums[l] > 0:
-            r -= 1
-        elif nums[r] + nums[m] + nums[l] < 0:
-            l += 1
-            m += 1
-        if nums[r] + nums[m] + nums[l] == 0:
-            res.append([nums[r], nums[m], nums[l]])
-            l += 1
-            m += 1
-    print(res)
