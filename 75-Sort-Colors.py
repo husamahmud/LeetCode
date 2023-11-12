8a928ad9746fa8d9d54bd9b646af340be3a2b830
iamhusam <husamahmud@gmail.com>
2023-11-12 21:47:42 +0300

    Add 75

---
+++ 75-Sort-Colors.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sortColors(self, nums):
+        n = len(nums)
+        for i in reversed(range(n)):
+            swapped = False
+            for j in range(i):
+                if nums[j] > nums[j + 1]:
+                    swapped = True
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+            if not swapped:
+                break
