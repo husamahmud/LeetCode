ce4d83403d0feb0f29d670564e85380a551ed58e
husamahmud <husamahmud@gmail.com>
2023-11-30 20:23:12 +0300

    Add 349

--- 349-Intersection-of-Two-Arrays.py
+++ 349-Intersection-of-Two-Arrays.py
@@ -6,6 +6,12 @@ class Solution(object):
 
     def intersection(self, nums1, nums2):
         s1 = set(nums1)
+        s2 = set(nums2)
+
+        return s1 & s2
+
+    def intersection(self, nums1, nums2):
+        s1 = set(nums1)
         s2 = set()
 
         for num in nums2:
