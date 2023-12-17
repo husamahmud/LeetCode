018215597c3ffa6212c28dbd0907a08953214cba
husamahmud <husamahmud@gmail.com>
2023-12-17 15:55:45 +0300

    Add 206

---
+++ 206-Reverse-Linked-List.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ */
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
+}
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    let curr = head,¥
+        next = null,
+        prev = null;
+
+    while (curr !== null) {
+        next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    head = prev;
+    return prev;
+};
