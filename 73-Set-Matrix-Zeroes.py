37161b8006f7ad3ca4d902a735b90d8005c4ff83
Hüsam <husamahmud@gmail.com>
2024-01-25 15:34:48 +0200

    [73] [Runtime Beats 69.35%] [Memory Beats 62.00%]

---
+++ 73-Set-Matrix-Zeroes.py
@@ -0,0 +1,18 @@
+            }
+        }
+    }
+
+
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(row[i]==1 || col[j]==1)
+            {
+                matrix[i][j]=0;
+            }
+        }
+    }
+
+    
+}
