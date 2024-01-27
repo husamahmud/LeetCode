2df776a35e7a3fca36814ac6b69c54ac0162c5ec
Hüsam <husamahmud@gmail.com>
2024-01-28 01:01:10 +0200

    [2] [Time Beats: 97.47%] [Memory Beats: 37.22%]

--- 2-Add-Two-Numbers.py
+++ 2-Add-Two-Numbers.py
@@ -7,23 +7,19 @@ class ListNode(object):
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         dummy = ListNode()
-        curr = dummy
+        curr = dummy 
         carry = 0
 
         while l1 or l2 or carry:
-            # get the values of the current digits
             num1 = l1.val if l1 else 0
             num2 = l2.val if l2 else 0
 
-            # caculate the sum and curry
             total = num1 + num2 + carry
             carry = total // 10
 
-            # create a new node with the summition of digits
             curr.next = ListNode(total % 10)
             curr = curr.next
 
-            # move to the next nodes in both lists
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
 
