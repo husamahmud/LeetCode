2b6b471fe6af2721cac3a0474fb7c33b5b6d9812
Hüsam <husamahmud@gmail.com>
2024-01-29 00:11:32 +0200

    [1] [Time Beats: 24.96%] [Memory Beats: 92.83%]

---
+++ 1-Two-Sum.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                result[0] = map.get(complement);
+                result[1] = i;
+                break;
+            }
+            map.put(nums[i], i);
+        }
+
+        return result;
+    }
+}
