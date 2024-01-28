07db8ccae95b7f2294c8bb63f68e1940e62b1483
husamahmud <husamahmud@gmail.com>
2024-01-29 01:10:14 +0300

    del

--- 1-Two-Sum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in map:
-                return [map[complement], i]
-            map[num] = i
