a31aa069429f5f8383ae652ef1472ecfef4fc9b3
Hüsam <husamahmud@gmail.com>
2024-02-01 16:12:07 +0300

    [1385] [Time Beats: 96.43%] [Memory Beats: 64.29%] - LeetPush

--- 1385-Find-the-Distance-Value-Between-Two-Arrays.py
+++ 1385-Find-the-Distance-Value-Between-Two-Arrays.py
@@ -1,1 +1,15 @@
-null
+class Solution(object):
+    def findTheDistanceValue(self, arr1, arr2, d):
+        count = 0
+
+        for num1 in arr1:
+            close = False
+            for num2 in arr2:
+                dist = abs(num1 - num2)        
+                if dist <= d:
+                    close = True
+                    break
+            if not close:
+                count += 1
+
+        return count
