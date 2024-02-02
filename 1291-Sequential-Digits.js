817f68f180efaeaf8d0a75996d1332e7bfbe0bd1
Hüsam <husamahmud@gmail.com>
2024-02-02 21:16:16 +0300

    [1291] [Time Beats: 10.00%] [Memory Beats: 82.50%] - LeetPush

--- 1291-Sequential-Digits.js
+++ 1291-Sequential-Digits.js
@@ -1,16 +1,20 @@
-10
-720133888
-356481080
-400352825
-1000000000
-1000000000
-45678
-45678
-1235
-2345679
-22
-77
-456789
-12345678
-24
-88
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number[]}
+ */
+var sequentialDigits = function(low, high) {
+    let s = "123456789";
+    let res = [];
+
+    for (let i = 0; i < s.length; i++) {
+        for (let j = i + 1; j < s.length; j++) {
+            let num = parseInt(s.substring(i, j + 1));
+
+            if (num > high) break;
+            if (low <= num) res.push(num);
+        }
+    }
+
+    return res.sort((a, b) => a - b);    
+};
