7ba2c6d7591e13a01fa8b5b6d6cc6b3658d86361
Hüsam <husamahmud@gmail.com>
2024-02-02 19:31:15 +0300

    [136] [Time Beats: 83.23%] [Memory Beats: 81.94%] - LeetPush

--- 136-Single-Number.py
+++ 136-Single-Number.py
@@ -1,26 +1,6 @@
 class Solution(object):
     def singleNumber(self, nums):
-        uniq = set()
-        for num in nums:
-            if num not in uniq:
-                uniq.add(num)
-            else:
-                uniq.remove(num)
-        return list(uniq)[0]
+        sum_of_nums = sum(nums)
+        sum_of_uniq = sum(set(nums))
 
-    def singleNumber(self, nums):
-        res = 0
-        for num in nums:
-            res ^= num
-        return res
-
-    def singleNumber(self, nums):
-        for num in nums:
-            if nums.count(num) == 1:
-                return num
-
-    def singleNumber(self, nums):
-        nums.sort()
-        for i in range(0, len(nums), 2):
-            if i + 1 == len(nums) or nums[i] != nums[i + 1]:
-                return nums[i]
+        return 2 * sum_of_uniq - sum_of_nums
