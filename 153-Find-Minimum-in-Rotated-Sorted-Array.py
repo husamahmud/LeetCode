d202f2828105f9ca2b969a4ec633dd0a370a79fe
Hüsam <husamahmud@gmail.com>
2024-02-06 17:03:08 +0300

    [153] [Time Beats: 89.60%] [Memory Beats: 50.63%] - LeetPush

--- 153-Find-Minimum-in-Rotated-Sorted-Array.py
+++ 153-Find-Minimum-in-Rotated-Sorted-Array.py
@@ -1,13 +1,11 @@
 class Solution(object):
     def findMin(self, nums):
         l = 0
-        r = len(nums) - 1
-
+        r = len(nums) - 1       
         while l < r:
             m = (l + r) // 2
             if nums[m] < nums[r]:
                 r = m
             else:
                 l = m + 1
-
         return nums[l]
