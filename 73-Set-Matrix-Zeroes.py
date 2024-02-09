39ae3bc667793815963e19d4dd33fd9151bbc00f
Hüsam <husamahmud@gmail.com>
2024-02-09 03:58:13 +0300

    [73] [Time Beats: 86.28%] [Memory Beats: 96.86%] - LeetPush

--- 73-Set-Matrix-Zeroes.py
+++ 73-Set-Matrix-Zeroes.py
@@ -1,18 +1,15 @@
-            }
-        }
-    }
+class Solution(object):
+    def setZeroes(self, matrix):
+        zero_rows = set()
+        zero_cols = set()
 
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    zero_rows.add(i)
+                    zero_cols.add(j)
 
-    for(int i=0;i<m;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            if(row[i]==1 || col[j]==1)
-            {
-                matrix[i][j]=0;
-            }
-        }
-    }
-
-    
-}
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i in zero_rows or j in zero_cols:
+                    matrix[i][j] = 0
