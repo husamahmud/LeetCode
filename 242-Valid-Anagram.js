4da76b973ceece2e74ad7ae3110f21a7987c7ee1
Hüsam <husamahmud@gmail.com>
2024-03-09 15:30:58 +0300

    [242] [Time Beats: 42.38%] [Memory Beats: 99.09%] - LeetPush

--- 242-Valid-Anagram.js
+++ 242-Valid-Anagram.js
@@ -3,36 +3,16 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function (s, t) {
-    if (s.length !== t.length)
-        return false
-    return s.split('').sort().join('') === t.split('').sort().join('')
+var isAnagram = function(s, t) {
+  if (s.length !== t.length) return false;
+
+  const freq = {};
+  for (let c of s) freq[c] = (freq[c] || 0) + 1;
+
+  for (let c of t) {
+    if (!freq[c]) return false;
+    freq[c]--;
+  }
+
+  return true;
 };
-
-var isAnagram = function (s, t) {
-    if (s.length !== t.length)
-        return false
-
-    const count = {}
-
-    for (let c of s)
-        count[c] = (count[c] || 0) + 1
-
-    console.log(count)
-
-    for (let c of t) {
-        if (count[c] === null)
-            return false
-        count[c]--
-    }
-
-    console.log(count)
-
-    for (let key in count)
-        if (count[key] !== 0)
-            return false
-
-    return true
-};
-
-console.log(isAnagram("rat", "car"))
