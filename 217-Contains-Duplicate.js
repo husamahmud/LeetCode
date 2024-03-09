9c71d84b95f0ded0e6d5d2cec0a8e6b7e71f09f2
Hüsam <husamahmud@gmail.com>
2024-03-09 14:58:24 +0300

    [217] [Time Beats: 52.00%] [Memory Beats: 80.18%] - LeetPush

--- 217-Contains-Duplicate.js
+++ 217-Contains-Duplicate.js
@@ -3,27 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-    return nums.length !== (new Set(nums)).size
+  const freq = {};
+
+  for (let num of nums) {
+    if (freq[num]) return true;
+    else freq[num] = 1;
+  }
+
+  return false;
 };
-
-var containsDuplicate = function (nums) {
-    const map = new Map()
-
-    for (let num of nums) {
-        if (map.has(num))
-            return true
-        map.set(num, true)
-    }
-
-    return false
-};
-
-var containsDuplicate = function (nums) {
-    nums.sort()
-
-    for (let i = 1; i < nums.length; i++)
-        if (nums[i] === nums[i - 1])
-            return true
-
-    return false
-}
