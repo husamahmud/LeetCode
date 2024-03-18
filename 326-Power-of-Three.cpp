1707df6b04c33ce9cc8b0c042c3e59b9fc818492
Hüsam <husamahmud@gmail.com>
2024-03-18 21:28:57 +0300

    [326] [Time Beats: 37.50%] [Memory Beats: 98.13%] - LeetPush

---
+++ 326-Power-of-Three.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+         if (n == 1)
+            return true;
+        else if (n < 1 || n % 3 != 0)
+            return false;
+
+        return isPowerOfThree(n / 3);
+    }
+};
