66cd485eb4ae545d93278fcd239de78410a55b87
Hüsam <husamahmud@gmail.com>
2024-03-27 22:05:13 +0300

    [17] [Time Beats: 46.70%] [Memory Beats: 100.00%] - LeetPush

--- 17-Letter-Combinations-of-a-Phone-Number.cpp
+++ 17-Letter-Combinations-of-a-Phone-Number.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-	vector <string> letterCombinations(string digits) {
-		if (digits.empty()) {
-			return {};
-		}
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty())
+            return {};
 
-		vector <string> res;
-		unordered_map<char, string> phoneMap = {{'2', "abc"},
-		                                        {'3', "def"},
-		                                        {'4', "ghi"},
-		                                        {'5', "jkl"},
-		                                        {'6', "mno"},
-		                                        {'7', "pqrs"},
-		                                        {'8', "tuv"},
-		                                        {'9', "wxyz"}};
+        vector<string> res;
+        unordered_map<char, string> phoneMap = {
+            {'2', "abc"}, {'3', "def"},  {'4', "ghi"}, {'5', "jkl"},
+            {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
 
-		backtrack(res, "", digits, phoneMap, 0);
-		return res;
-	}
+        bt(res, "", digits, phoneMap, 0);
+        return res;
+    }
 
 private:
-	void
-	backtrack(vector <string> &res, string comb, string digits, unordered_map<char, string> &phoneMap, int idx) {
-		if (idx == digits.length()) {
-			res.push_back(comb);
-			return;
-		}
+    void bt(vector<string>& res, string comb, string digits,
+            unordered_map<char, string>& phoneMap, int i) {
+        if (i == digits.length()) {
+            res.push_back(comb);
+            return;
+        }
 
-		char digit = digits[idx];
-		string letters = phoneMap[digit];
+        char digit = digits[i];
+        string letters = phoneMap[digit];
 
-		for (char letter: letters) {
-			backtrack(res, comb + letter, digits, phoneMap, idx + 1);
-		}
-	}
+        for (char letter : letters)
+            bt(res, comb + letter, digits, phoneMap, i + 1);
+    }
 };
