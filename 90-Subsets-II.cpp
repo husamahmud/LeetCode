d939fdb8b00e6e1ca97aeaad97688743cd2e43ec
Hüsam <husamahmud@gmail.com>
2024-03-27 21:42:53 +0300

    [90] [Time Beats: 20.93%] [Memory Beats: 48.62%] - LeetPush

--- 90-Subsets-II.cpp
+++ 90-Subsets-II.cpp
@@ -5,25 +5,24 @@ public:
         vector<int> subset;
 
         sort(nums.begin(), nums.end());
-        backtrack(nums, res, subset, 0);
-
+        bt(nums, res, subset, 0);
         return res;
     }
 
 private:
-    void backtrack(vector<int>& nums, vector<vector<int>>& res, vector<int>& subset, int i) {
+    void bt(vector<int> nums, vector<vector<int>>& res, vector<int>& subset, int i) {
         if (nums.size() == i) {
             res.push_back(subset);
             return;
         }
 
         subset.push_back(nums[i]);
-        backtrack(nums, res, subset, i + 1);
+        bt(nums, res, subset, i + 1);
         subset.pop_back();
 
         while (i + 1 < nums.size() && nums[i] == nums[i + 1])
             i++;
 
-        backtrack(nums, res, subset, i + 1);
+        bt(nums, res, subset, i + 1);
     }
 };
