ea8106fc525ed27a2d8c3808b0d8cf125704aac9
Hüsam <husamahmud@gmail.com>
2024-03-31 07:21:17 +0300

    [1] [Time Beats: 17.04%] [Memory Beats: 79.56%] - LeetPush

--- 1-Two-Sum.cpp
+++ 1-Two-Sum.cpp
@@ -1,16 +1,1 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> map;
-        int diff, i;
-
-        for (i = 0; i < nums.size(); i++) {
-            diff = target - nums[i];
-            if (map.count(diff))
-                return {map[diff], i};
-            map[nums[i]] = i;
-        }
-
-        return {-1};
-    }
-};
+null
