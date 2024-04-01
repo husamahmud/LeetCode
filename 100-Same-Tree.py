ab026a52e27a5f8ed86d163950e2b6199d43d2e0
Hüsam <husamahmud@gmail.com>
2024-04-01 07:56:58 +0300

    [100] [Time Beats: 42.56%] [Memory Beats: 76.00%] - LeetPush

--- 100-Same-Tree.py
+++ 100-Same-Tree.py
@@ -1,18 +1,19 @@
-def __init__(self, val=0, left=None, right=None):
-    self.val = val
-    self.left = left
-    self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-
-class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+class Solution(object):
+    def isSameTree(self, p, q):
         if not p and not q:
             return True
         if not p or not q:
             return False
 
         return (
-            p.val == q.val
-            and self.isSameTree(p.left, q.left)
-            and self.isSameTree(p.right, q.right)
+            p.val == q.val and 
+            self.isSameTree(p.left, q.left) and
+            self.isSameTree(p.right, q.right)
         )
+        
