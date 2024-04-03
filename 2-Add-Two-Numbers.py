b382aabe738b59c4b6cbed6880c1e6dd3f5a1504
Hüsam <husamahmud@gmail.com>
2024-04-03 11:02:25 +0300

    [2] [Time Beats: 94.99%] [Memory Beats: 87.09%] - LeetPush

--- 2-Add-Two-Numbers.py
+++ 2-Add-Two-Numbers.py
@@ -1,9 +1,3 @@
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         dummy = ListNode()
