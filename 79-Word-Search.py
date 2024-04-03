3454504f0c489609dff8cd3f20262f3b3b778a73
Hüsam <husamahmud@gmail.com>
2024-04-03 08:28:00 +0300

    [79] [Time Beats: 26.43%] [Memory Beats: 57.51%] - LeetPush

---
+++ 79-Word-Search.py
@@ -0,0 +1,28 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+        visited = set()
+
+        def dfs(r: int, c: int, idx: int):
+            if idx == len(word):
+                return True
+
+            if (r < 0 or c < 0 or r >= rows or c >= cols or
+                word[idx] != board[r][c] or 
+                (r, c) in visited):
+                return False
+
+            visited.add((r, c))
+            res = (dfs(r + 1, c, idx + 1) or
+                   dfs(r - 1, c, idx + 1) or
+                   dfs(r, c + 1, idx + 1) or
+                   dfs(r, c - 1, idx + 1))
+            visited.remove((r, c))
+            return res
+
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0):
+                    return True
+
+        return False
