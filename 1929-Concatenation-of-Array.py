b15b9702addd4920f1b28ede8d2aa9dd279fc963
Hüsam <husamahmud@gmail.com>
2024-04-19 15:51:42 +0300

    [1929] [Time Beats: 84.73%] [Memory Beats: 20.37%] - LeetPush

--- 1929-Concatenation-of-Array.py
+++ 1929-Concatenation-of-Array.py
@@ -1,15 +1,4 @@
-class Solution(object):
-	def getConcatenation(self, nums):
-		# 1
-		nums.extend(nums)
-		return nums
-		# 2
-		return nums + nums
-		# 3
-		return [*nums, *nums]
-		# 4
-		ans = []
-		for i in range(2):
-			for num in nums:
-				ans.append(num)
-		return ans
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        nums.extend(nums)
+        return nums
