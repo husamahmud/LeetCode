941ac4a77a1f62bad23e2c28c4041f3fd2201475
Hüsam <husamahmud@gmail.com>
2024-04-23 16:56:43 +0300

    [238] [Time Beats: 24.98%] [Memory Beats: 31.16%] - LeetPush

--- 238-Product-of-Array-Except-Self.py
+++ 238-Product-of-Array-Except-Self.py
@@ -1,33 +1,15 @@
-class Solution(object):
-	def productExceptSelf(self, nums):
-		n = len(nums)
-		res = [1] * n
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [1] * n
+        pre = [1] * n
 
-		pref = 1
-		for i in range(n):
-			res[i] *= pref
-			pref *= nums[i]
+        for i in range(1, n):
+            pre[i] = pre[i - 1] * nums[i - 1]
 
-		suff = 1
-		for i in range(n - 1, -1, -1):
-			res[i] *= suff
-			suff *= nums[i]
+        product = 1
+        for i in range(n - 1, -1, -1):
+            res[i] = product * pre[i]
+            product *= nums[i]
 
-		return res
-
-	def productExceptSelf(self, nums):
-		n = len(nums)
-		pref = [1] * n
-		suff = [1] * n
-		res = [1] * n
-
-		for i in range(1, n):
-			pref[i] = nums[i - 1] * pref[i - 1]
-
-		for i in range(n - 2, -1, -1):
-			suff[i] = nums[i + 1] * suff[i + 1]
-
-		for i in range(n):
-			res[i] = pref[i] * suff[i]
-
-		return res
+        return res
