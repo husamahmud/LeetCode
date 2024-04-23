ac7464eb136f2d7833384646b08a522e7345dce3
Hüsam <husamahmud@gmail.com>
2024-04-23 16:14:47 +0300

    [347] [Time Beats: 97.00%] [Memory Beats: 56.66%] - LeetPush

--- 347-Top-K-Frequent-Elements.py
+++ 347-Top-K-Frequent-Elements.py
@@ -1,17 +1,6 @@
-class Solution(object):
-	def topKFrequent(self, nums, k):
-		freq = {}
-		res = []
-
-		# count the frequency of each number in the nums list
-		for num in nums:
-			freq[num] = freq.get(num, 0) + 1
-
-		# sort the freq dic by its values
-		sorted_hash = sorted(freq.items(), key=lambda x: x[1], reverse=True)
-
-		# Append only the key
-		for i in range(k):
-			res.append(sorted_hash[i][0])
-
-		return res
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = Counter(nums)
+        res = [entry[0] for entry in sorted(freq.items(), key=lambda x: x[1], reverse=True)]
+        
+        return res[:k]
