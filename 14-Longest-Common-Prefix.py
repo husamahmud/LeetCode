13e605cce3c899a7d530f7fe210d7f2de5329c49
Hüsam <husamahmud@gmail.com>
2024-04-24 14:42:24 +0300

    [14] [Time Beats: 87.94%] [Memory Beats: 10.03%] - LeetPush

--- 14-Longest-Common-Prefix.py
+++ 14-Longest-Common-Prefix.py
@@ -1,15 +1,15 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        if strs is None:
-            return None
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        pre = \\
+        sortedStrs = sorted(strs, key=str.lower)
+        smallest = sortedStrs[0]
+        longest = sortedStrs[-1]
 
-        strs.sort()
-
-        prefix = ""
-        for i in range(len(strs[0])):
-            if strs[0][i] == strs[-1][i]:
-                prefix += strs[0][i]
+        for i in range(len(smallest)):
+            if smallest[i] == longest[i]:
+                pre += smallest[i]
             else:
-                break
+                return pre
 
-        return prefix
+        return pre
+        
