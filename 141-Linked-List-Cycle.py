5fdb67d1346509383dcb4769223b92cdbff79fd1
Hüsam <husamahmud@gmail.com>
2024-04-26 19:21:30 +0300

    [141] [Time Beats: 58.79%] [Memory Beats: 73.00%] - LeetPush

--- 141-Linked-List-Cycle.py
+++ 141-Linked-List-Cycle.py
@@ -1,27 +1,12 @@
-class Solution(object):
-    def hasCycle(self, head):  # fast and slow pointers
-        slow = head
-        fast = head
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return False
+
+        slow, fast = head, head.next
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+            slow, fast = slow.next, fast.next.next
             if slow == fast:
                 return True
-        return False
 
-    def hasCycle(self, head):  # using set
-        visited = set()
-        while head:
-            if head in visited:
-                return True
-            visited.add(head)
-            head = head.next
-        return False
-
-    def hasCycle(self, head):  # modifying the linked list
-        while head:
-            if head.val == float('inf'):
-                return True
-            head.val = float('inf')
-            head = head.next
         return False
