dd7ea7a499d7b04b8f9000e35211d95b69158df5
Hüsam <husamahmud@gmail.com>
2024-04-26 18:17:31 +0300

    [203] [Time Beats: 96.27%] [Memory Beats: 87.82%] - LeetPush

--- 203-Remove-Linked-List-Elements.py
+++ 203-Remove-Linked-List-Elements.py
@@ -1,35 +1,16 @@
 class Solution:
-    def removeContiguous(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        while head and head.val == val:
-            head = head.next
-        
-        if not head:
-            return None
-        
-        cur = head
-        while cur and cur.next:
-            if cur.next.val == val:
-                while cur.next and cur.next.val == val:
-                    cur.next = cur.next.next
-            else:
-                cur = cur.next
-        
-        return head
-
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
         if not head:
-            return head
-        
-        new_head = self.removeContiguous(head, val)
-        if not new_head:
             return None
-        
-        head = new_head
-        cur = head
-        while cur and cur.next:
-            if cur.next.val == val:
-                cur.next = cur.next.next
+
+        dummy = ListNode(val=0, next=head)
+        prev, curr = dummy, head
+
+        while curr:
+            if curr.val == val:
+                prev.next = curr.next
             else:
-                cur = cur.next
-        
-        return head
+                prev = curr
+            curr = curr.next
+
+        return dummy.next
