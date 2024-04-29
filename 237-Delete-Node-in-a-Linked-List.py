af63140eb9ad9bdfa5d8fee47b17941d6eb9c9a5
Hüsam <husamahmud@gmail.com>
2024-04-29 20:43:20 +0300

    [237] [Time Beats: 73.05%] [Memory Beats: 13.53%] - LeetPush

--- 237-Delete-Node-in-a-Linked-List.py
+++ 237-Delete-Node-in-a-Linked-List.py
@@ -1,12 +1,4 @@
 class Solution:
     def deleteNode(self, node):
-        deleted, curr = node, node
-
-        while deleted.next:
-            deleted.val, deleted.next.val = deleted.next.val, deleted.val
-            deleted = deleted.next
-
-        while curr.next != deleted:
-            curr = curr.next
-
-        curr.next = None
+        node.val = node.next.val
+        node.next = node.next.next
