2fdbcd7823580e391b5fa88f40545e98035b7485
Hüsam <husamahmud@gmail.com>
2024-04-30 22:14:04 +0300

    [20] [Time Beats: 77.20%] [Memory Beats: 74.60%] - LeetPush

--- 20-Valid-Parentheses.py
+++ 20-Valid-Parentheses.py
@@ -1,19 +1,19 @@
-class Solution(object):
-    def isValid(self, s):
-        stk = []
-        n = len(s)
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
 
-        for c in range(n):
-            if s[c] == '(' or s[c] == '{' or s[c] == '[':
-                stk.append(s[c])
-            else:
-                if len(stk) == 0:
-                    return False
+        for c in s:
+            if c == "(" or c == "[" or c == "{":
+                stack.append(c)
+                continue
 
-                top = stk.pop()
-                if (s[c] == ')' and top != '(') or \
-                   (s[c] == '}' and top != '{') or \
-                   (s[c] == ']' and top != '['):
-                        return False
+            if len(stack) == 0:
+                return False
 
-        return len(stk) == 0
+            top = stack.pop()
+            if top == '(' and c != ')' or \
+                top == '{' and c != '}' or \
+                top == '[' and c != ']':
+                return False
+
+        return len(stack) == 0
