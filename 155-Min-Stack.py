54cacf9d3c76d7effb58fb0740ac2ba7bbc3849a
Hüsam <husamahmud@gmail.com>
2024-04-30 23:26:07 +0300

    [155] [Time Beats: 86.02%] [Memory Beats: 55.81%] - LeetPush

--- 155-Min-Stack.py
+++ 155-Min-Stack.py
@@ -1,27 +1,19 @@
 class MinStack:
     def __init__(self):
-        self.stk = []
-        self.min_val = float('inf')
+        self.stack = []
+        self.minStack = []
 
-    def push(self, val):
-        if val < self.min_val:
-            self.min_val = val
-        self.stk.append(val)
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
 
-    def pop(self):
-        last_num = self.stk.pop()
-        if last_num == self.min_val:
-            self.update_min()
-        return last_num
+    def pop(self) -> None:
+        self.minStack.pop()
+        self.stack.pop()
 
-    def top(self):
-        return self.stk[-1]
+    def top(self) -> int:
+        return self.stack[-1]
 
-    def getMin(self):
-        return self.min_val
-
-    def update_min(self):
-        self.min_val = float('inf')
-        for num in self.stk:
-            if num < self.min_val:
-                self.min_val = num
+    def getMin(self) -> int:
+        return self.minStack[-1]
