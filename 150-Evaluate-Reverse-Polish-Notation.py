045a23d66eea8801e916974c475470cc15cb5423
Hüsam <husamahmud@gmail.com>
2024-05-01 22:17:36 +0300

    [150] [Time Beats: 77.86%] [Memory Beats: 84.51%] - LeetPush

--- 150-Evaluate-Reverse-Polish-Notation.py
+++ 150-Evaluate-Reverse-Polish-Notation.py
@@ -1,19 +1,19 @@
 class Solution(object):
     def calc(slef, op, x, y):
-        if op == '+':
+        if op == "+":
             return x + y
-        elif op == '-':
+        elif op == "-":
             return x - y
-        elif op == '*':
+        elif op == "*":
             return x * y
-        elif op == '/':
+        elif op == "/":
             return int(float(x) / y)
 
     def evalRPN(self, tokens):
         stack = []
 
         for token in tokens:
-            if token.isdigit() or (token[0] == '-' and token[1:].isdigit()):
+            if token.isdigit() or (token[0] == "-" and token[1:].isdigit()):
                 stack.append(int(token))
             else:
                 y = stack.pop()
