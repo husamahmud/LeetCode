ea5fb977699323eea233745502657ece74a22033
Hüsam <husamahmud@gmail.com>
2024-05-02 17:38:16 +0300

    [36] [Time Beats: 73.98%] [Memory Beats: 21.18%] - LeetPush

--- 36-Valid-Sudoku.py
+++ 36-Valid-Sudoku.py
@@ -1,37 +1,22 @@
-class Solution(object):
-	def isValidSudoku(slef, board):
-		# check rows
-		for i in range(9):
-			row_nums = set()
-			for j in range(9):
-				cell = board[i][j]
-				if cell != '.':
-					if cell in row_nums:
-						return False
-					else:
-						row_nums.add(cell)
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
 
-		# check columns
-		for j in range(9):
-			col_nums = set()
-			for i in range(9):
-				cell = board[i][j]
-				if cell != '.':
-					if cell in col_nums:
-						return False
-					else:
-						col_nums.add(cell)
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == \.\:
+                    continue
+                if (
+                    board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]
+                ):
+                    return False
 
-		# check 3x3 sub-boxes
-		for box in range(9):
-			box_nums = set()
-			for i in range(box // 3 * 3, box // 3 * 3 + 3):
-				for j in range(box % 3 * 3, box % 3 * 3 + 3):
-					cell = board[i][j]
-					if cell != '.':
-						if cell in box_nums:
-							return False
-						else:
-							box_nums.add(cell)
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
 
-		return True
+        return True
