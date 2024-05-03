cf85f08d8158f9c95d4db78bedabd665c1b0e0bd
Hüsam <husamahmud@gmail.com>
2024-05-03 18:30:33 +0300

    [344] [Time Beats: 60.40%] [Memory Beats: 93.49%] - LeetPush

--- 344-Reverse-String.py
+++ 344-Reverse-String.py
@@ -1,13 +1,8 @@
-if __name__ == '__main__':
-	s = ["h", "e", "l", "l", "o"]
-	l = 0
-	r = len(s) - 1
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        l, r = 0, len(s) - 1
 
-	while l < r:
-		temp = s[l]
-		s[l] = s[r]
-		s[r] = temp
-		l += 1
-		r -= 1
-
-	print(s)
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
