d4b55743669c8dd8d2296f9a48ceb4deb5c2f741
Hüsam <husamahmud@gmail.com>
2024-05-03 16:15:25 +0300

    [125] [Time Beats: 73.14%] [Memory Beats: 63.29%] - LeetPush

--- 125-Valid-Palindrome.py
+++ 125-Valid-Palindrome.py
@@ -1,18 +1,18 @@
-class Solution(object):
-	def isPalindrome(self, s):
-		l = 0
-		r = len(s) - 1
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
 
-		while l < r:
-			if not s[l].isalnum():
-				l += 1
-				continue
-			if not s[r].isalnum():
-				r -= 1
-				continue
-			if s[l].lower() != s[r].lower():
-				return False
-			l += 1
-			r -= 1
-
-		return True
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+                continue
+            if not s[r].isalnum():
+                r -= 1
+                continue
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        
+        return True
+            
