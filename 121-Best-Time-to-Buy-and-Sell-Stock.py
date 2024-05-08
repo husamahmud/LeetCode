faf17212c62b09223f8551a70bf6cef40e9ae589
Hüsam <husamahmud@gmail.com>
2024-05-08 17:05:34 +0300

    [121] [Time Beats: 56.16%] [Memory Beats: 95.25%] - LeetPush

--- 121-Best-Time-to-Buy-and-Sell-Stock.py
+++ 121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -1,14 +1,13 @@
-class Solution(object):
-    def maxProfit(self, prices):
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         l, r = 0, 1
-        max_profit = 0
+        maxP = 0
 
         while r < len(prices):
             if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                max_profit = max(max_profit, profit)
+                maxP = max(maxP, prices[r] - prices[l])
             else:
                 l = r
             r += 1
-
-        return max_profit
+        
+        return maxP
