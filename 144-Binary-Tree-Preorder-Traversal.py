b274645c90051a8f8d16c6ec2161acd336e9828d
Hüsam <husamahmud@gmail.com>
2024-05-11 22:53:33 +0300

    [144] [Time Beats: 93.45%] [Memory Beats: 80.40%] - LeetPush

--- 144-Binary-Tree-Preorder-Traversal.py
+++ 144-Binary-Tree-Preorder-Traversal.py
@@ -1,12 +1,15 @@
-class TreeNode(object):
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-class Solution(object):
-    def preorderTraversal(self, root):
-        if root is None:
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
             return []
 
-        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+        res = []
+        def traversal(root: TreeNode) -> None:
+            if not root:
+                return
+            res.append(root.val)
+            traversal(root.left)
+            traversal(root.right)
+        
+        traversal(root)
+        return res        
