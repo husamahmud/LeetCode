a8f9cd9a1495f67cbb2d458d6eb1f6e31e99627b
Hüsam <husamahmud@gmail.com>
2024-05-11 23:42:32 +0300

    [226] [Time Beats: 67.55%] [Memory Beats: 34.26%] - LeetPush

--- 226-Invert-Binary-Tree.py
+++ 226-Invert-Binary-Tree.py
@@ -1,15 +1,8 @@
-class TreeNode(object):
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
-class Solution(object):
-    def invertTree(self, root):
-        if root is None:
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
             return
 
-        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)        
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
 
         return root
