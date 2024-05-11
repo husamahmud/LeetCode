f9662a8488077994fad05161570a7e4dbb927771
Hüsam <husamahmud@gmail.com>
2024-05-11 22:52:34 +0300

    [94] [Time Beats: 96.41%] [Memory Beats: 24.75%] - LeetPush

--- 94-Binary-Tree-Inorder-Traversal.py
+++ 94-Binary-Tree-Inorder-Traversal.py
@@ -1,12 +1,16 @@
-class TreeNode(object):
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-class Solution(object):
-    def inorderTraversal(self, root):
-        if root is None:
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
             return []
 
-        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+        res = []
+        def traversal(root: TreeNode) -> None:
+            if not root:
+                return
+            traversal(root.left)
+            res.append(root.val)
+            traversal(root.right)
+        
+        traversal(root)
+        return res
+            
