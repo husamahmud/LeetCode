6830778d9e220e68c2c36ff4bfe47b57125b4e8b
Hüsam <husamahmud@gmail.com>
2024-05-15 16:27:26 +0300

    [143] [Time Beats: 94.16%] [Memory Beats: 50.22%] - LeetPush

--- 143-Reorder-List.py
+++ 143-Reorder-List.py
@@ -1,25 +1,19 @@
 class Solution:
-    def rearrangeList(self, arr: List) -> List:
-        res = []
-        l, r = 0, len(arr) - 1
-
-        while l <= r:
-            res.append(arr[l])
-            if l != r:
-                res.append(arr[r])
-            l += 1
-            r -= 1
-
-        return res
-
     def reorderList(self, head: Optional[ListNode]) -> None:
-        nodes, curr = [], head
+        curr, nodes = head, []
 
         while curr:
             nodes.append(curr.val)
             curr = curr.next
 
-        nodes = self.rearrangeList(nodes)
-        for node in nodes:
+        newNodes = []
+        l, r = 0, len(nodes) - 1
+        while l <= r:
+            newNodes.append(nodes[l])
+            if l < r:
+                newNodes.append(nodes[r])
+            l, r = l + 1, r - 1
+
+        for node in newNodes:
             head.val = node
             head = head.next
