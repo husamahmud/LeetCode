a1d04916dbcb742a08625444dc1f97206fcc8c5d
Hüsam <husamahmud@gmail.com>
2024-05-15 08:56:28 +0300

    [83] [Time Beats: 26.34%] [Memory Beats: 11.13%] - LeetPush

--- 83-Remove-Duplicates-from-Sorted-List.py
+++ 83-Remove-Duplicates-from-Sorted-List.py
@@ -1,28 +1,11 @@
 class Solution:
-    def removeContiguous(self, head: ListNode) -> ListNode:
-        curr, next = head, head.next
-        
-        while next and curr.val == next.val:
-            next = next.next
-        
-        curr.next = next
-        return curr
-
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
-            return head
+        curr = head
         
-        if head.val == head.next.val:
-            new_head = self.removeContiguous(head)
-            head = new_head
-        
-        curr, next = head, head.next
-        while next:
-            if next.val == curr.val:
-                curr = self.removeContiguous(curr)
-                next = curr.next
-            else:
-                next, curr = next.next, curr.next
-        
+        while curr:
+            while curr.next and curr.val == curr.next.val:
+                curr.next = curr.next.next
+            curr = curr.next
+
         return head
-        
+            
