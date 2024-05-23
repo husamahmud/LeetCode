b37400ac3421d772db9cbae0b30ff7bcd85548c9
Hüsam <husamahmud@gmail.com>
2024-05-23 15:53:41 +0300

    [977] [Time Beats: 61.28%] [Memory Beats: 58.19%] - LeetPush

---
+++ 977-Squares-of-a-Sorted-Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [0] * n
+        l, r = 0, n - 1
+
+        for i in range(n - 1, -1, -1):
+            if abs(nums[l]) > abs(nums[r]):
+                res[i] = nums[l] ** 2
+                l += 1
+            else:
+                res[i] = nums[r] ** 2
+                r -= 1
+
+        return res
