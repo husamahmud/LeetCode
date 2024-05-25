423cbb6c91ed469061937ebaa64a8b1725b57bbc
Hüsam <husamahmud@gmail.com>
2024-05-25 16:49:24 +0300

    [69] [Time Beats: 51.33%] [Memory Beats: 40.86%] - LeetPush

---
+++ 69-Sqrt(x).py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r = 0, x
+
+        while l <= r:
+            m = l + (r - l) // 2
+
+            if m**2 == x:
+                return m
+            elif m**2 > x:
+                r = m - 1
+            else:
+                l = m + 1
+
+        return r
