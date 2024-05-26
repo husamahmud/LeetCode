2049cf79575785c8f0d969871ed55b0d61a87952
Hüsam <husamahmud@gmail.com>
2024-05-26 12:45:46 +0300

    [104] [Time Beats: 89.39%] [Memory Beats: 97.72%] - LeetPush

--- 104-Maximum-Depth-of-Binary-Tree.py
+++ 104-Maximum-Depth-of-Binary-Tree.py
@@ -1,24 +1,8 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        # Base case
         if not root:
             return 0
-
-        # Sol1 DFS
-        # return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
-
-        # Sol2 BFS level traversal
-        level = 0
-        q = deque([root])
-
-        while q:
-            for i in range(len(q)):
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            level += 1
-        
-        return level
-
+        else:
+            lDepth = self.maxDepth(root.left)
+            rDepth = self.maxDepth(root.right)
+            return max(lDepth, rDepth) + 1
