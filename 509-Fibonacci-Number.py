156f4ab70130bb6d767f36fc9991394857edfbf9
Hüsam <husamahmud@gmail.com>
2024-05-28 16:04:01 +0300

    [509] [Time Beats: 69.02%] [Memory Beats: 80.76%] - LeetPush

--- 509-Fibonacci-Number.py
+++ 509-Fibonacci-Number.py
@@ -1,12 +1,31 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0 or n == 1:
-            return n
+        # 3: Recursive solution optimized
+        memo = {}
 
-        memo = [0] * n
-        memo[0], memo[1] = 0, 1
+        def fib_memo(i: int) -> int:
+            if i == 0 or i == 1:
+                return i
+            if i in memo:
+                return memo[i]
 
-        for i in range(2, n):
-            memo[i] = memo[i - 1] + memo[i - 2]
+            res = fib_memo(i - 1) + fib_memo(i - 2)
+            memo[i] = res
+            return res
 
-        return memo[n - 1] + memo[n - 2]
+        return fib_memo(n)
+
+        # 2: Recursive solution
+        # if n == 0 or n == 1:
+        #     return n
+        # return self.fib(n - 1) + self.fib(n - 2)
+
+        # 1: Iterative solutions
+        # if n == 0 or n == 1:
+        #     return n
+        # memo = [0] * n
+        # memo[0], memo[1] = 0, 1
+
+        # for i in range(2, n):
+        #     memo[i] = memo[i - 1] + memo[i - 2]
+        # return memo[n - 1] + memo[n - 2]
