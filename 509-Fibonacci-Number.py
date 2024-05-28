a1ca0fdbc4debf784bacf645430ec1fa295dfc35
Hüsam <husamahmud@gmail.com>
2024-05-28 15:58:19 +0300

    [509] [Time Beats: 98.59%] [Memory Beats: 80.76%] - LeetPush

--- 509-Fibonacci-Number.py
+++ 509-Fibonacci-Number.py
@@ -1,5 +1,12 @@
-class Solution(object):
-    def fib(self, n):
-        if n <= 1:
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0 or n == 1:
             return n
-        return self.fib(n - 1) + self.fib(n - 2)
+
+        memo = [0] * n
+        memo[0], memo[1] = 0, 1
+
+        for i in range(2, n):
+            memo[i] = memo[i - 1] + memo[i - 2]
+
+        return memo[n - 1] + memo[n - 2]
