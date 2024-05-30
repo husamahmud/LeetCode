c9bc39d6135abe4a59d967fa4e3a6312e4c047c4
Hüsam <husamahmud@gmail.com>
2024-05-30 19:15:22 +0300

    [22] [Time Beats: 66.95%] [Memory Beats: 86.41%] - LeetPush

--- 22-Generate-Parentheses.py
+++ 22-Generate-Parentheses.py
@@ -3,34 +3,35 @@ class Solution:
         res = []
 
         def generate(cur: str, opened: int, closed: int) -> None:
-            if len(cur) == 2 * n:
+            if 2 * n == len(cur):
                 res.append(cur)
                 return
-            
+
             if opened < n:
                 generate(cur + "(", opened + 1, closed)
-            if closed < opened:
+            if opened > closed:
                 generate(cur + ")", opened, closed + 1)
 
         generate("", 0, 0)
         return res
 
-    # BruteForce Solution
     # def generateParenthesis(self, n: int) -> List[str]:
     #     res = []
+
     #     def generate(cur: str) -> None:
-    #         if len(cur) == 2 * n:
+    #         if 2 * n == len(cur):
     #             if isValid(cur):
     #                 res.append(cur)
     #             return
     #         generate(cur + "(")
     #         generate(cur + ")")
+
     #     def isValid(s: str) -> bool:
     #         balance = 0
     #         for c in s:
     #             if c == "(":
     #                 balance += 1
-    #             elif c == ")":
+    #             else:
     #                 balance -= 1
     #             if balance < 0:
     #                 return False
