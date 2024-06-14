16ab13d315c4915eb0bfb9c74cbefc06bfabf5df
Hüsam <husamahmud@gmail.com>
2024-06-14 21:47:02 +0300

    [22] [Time Beats: undefined] [Memory Beats: undefined] - LeetPush

--- 22-Generate-Parentheses.py
+++ 22-Generate-Parentheses.py
@@ -2,39 +2,27 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
 
-        def generate(cur: str, opened: int, closed: int) -> None:
-            if 2 * n == len(cur):
-                res.append(cur)
+        def generate(curr: str) -> None:
+            if len(curr) == n * 2:
+                if isValid(curr):
+                    res.append(curr)
                 return
 
-            if opened < n:
-                generate(cur + "(", opened + 1, closed)
-            if opened > closed:
-                generate(cur + ")", opened, closed + 1)
+            generate(curr + "(")
+            generate(curr + ")")
 
-        generate("", 0, 0)
+        def isValid(s: str) -> bool:
+            balance = 0
+
+            for c in s:
+                if c == "(":
+                    balance += 1
+                elif c == ")":
+                    balance -= 1
+                if balance < 0:
+                    return False
+
+            return balance == 0
+
+        generate("")
         return res
-
-    # def generateParenthesis(self, n: int) -> List[str]:
-    #     res = []
-
-    #     def generate(cur: str) -> None:
-    #         if 2 * n == len(cur):
-    #             if isValid(cur):
-    #                 res.append(cur)
-    #             return
-    #         generate(cur + "(")
-    #         generate(cur + ")")
-
-    #     def isValid(s: str) -> bool:
-    #         balance = 0
-    #         for c in s:
-    #             if c == "(":
-    #                 balance += 1
-    #             else:
-    #                 balance -= 1
-    #             if balance < 0:
-    #                 return False
-    #         return balance == 0
-    #     generate("")
-    #     return res
