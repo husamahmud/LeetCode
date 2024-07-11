03a0f40a35ca764195acec0d16310e570d0bc034
Hüsam <husamahmud@gmail.com>
2024-07-12 02:10:49 +0300

    [1] [Time Beats: 72.50%] [Memory Beats: 46.71%] - LeetPush

--- 1-Two-Sum.js
+++ 1-Two-Sum.js
@@ -3,14 +3,14 @@
  * @param {number} target
  * @return {number[]}
  */
-const twoSum = (nums, target) => {
-    const map = new Map();
+var twoSum = function (nums, target) {
+    const map = {}
+
     for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        if (map.has(complement)) {
-            return [map.get(complement), i];
-        }
-        map.set(nums[i], i);
+        const diff = target - nums[i]
+        if (diff in map) return [i, map[diff]]
+        else map[nums[i]] = i
     }
-    return [];
+
+    return []
 };
