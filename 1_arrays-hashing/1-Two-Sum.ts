0a1f65dc73b4a3bbf31d97babbf19b5408082c1b
Hüsam <husamahmud@gmail.com>
2024-07-30 01:58:56 +0300

    [1] [Time Beats: 81.59%] [Memory Beats: 44.63%] - LeetPush

---
+++ 1_arrays-hashing/1-Two-Sum.ts
@@ -0,0 +1,12 @@
+function twoSum(nums: number[], target: number): number[] {
+  const m = {}
+
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i]
+
+    if (diff in m) return [i, m[diff]]
+    m[nums[i]] = i
+  }
+
+  return [-1]
+};
