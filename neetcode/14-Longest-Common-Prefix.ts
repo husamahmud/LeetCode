ef5fc81d7d0d5154292adc6ee2fd740763c1e829
Hüsam <husamahmud@gmail.com>
2024-10-07 13:28:45 +0300

    [14] [Time Beats: 78.33%] [Memory Beats: 65.42%] - LeetPush

---
+++ neetcode/14-Longest-Common-Prefix.ts
@@ -0,0 +1,15 @@
+function longestCommonPrefix(strs: string[]): string {
+  if (!strs) return ''
+  let res = "";
+
+  strs.sort((a: string, b: string) => a.length - b.length)
+
+  for (let i = 0; i < strs[0].length; i++) {
+    let c = strs[0][i]
+
+    if (strs.every((str: string) => str[i] === c)) res += c
+    else return res
+  }
+
+  return res
+};
