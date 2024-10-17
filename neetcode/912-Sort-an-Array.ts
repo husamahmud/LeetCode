9074a92c29549be4a322d109cb69851eb3d8d764
Hüsam <husamahmud@gmail.com>
2024-10-17 14:05:56 +0300

    [912] [Time Beats: 29.28%] [Memory Beats: 34.68%] - LeetPush

---
+++ neetcode/912-Sort-an-Array.ts
@@ -0,0 +1,28 @@
+function merge(left: number[], right: number[]): number[] {
+  let result: number[] = [];
+  let i = 0, j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+
+  return result.concat(left.slice(i)).concat(right.slice(j));
+}
+
+function sortArray(nums: number[]): number[] {
+  if (nums.length <= 1) {
+    return nums;
+  }
+
+  let mid = Math.floor(nums.length / 2);
+  let left = sortArray(nums.slice(0, mid));
+  let right = sortArray(nums.slice(mid));
+
+  return merge(left, right);
+}
