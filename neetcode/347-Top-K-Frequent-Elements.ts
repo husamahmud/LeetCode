a5f8d0343be39eb4e7bb7287f821478c41baf48f
Hüsam <husamahmud@gmail.com>
2024-10-17 14:40:46 +0300

    [347] [Time Beats: 100.00%] [Memory Beats: 30.10%] - LeetPush

--- neetcode/347-Top-K-Frequent-Elements.ts
+++ neetcode/347-Top-K-Frequent-Elements.ts
@@ -1,11 +1,20 @@
 function topKFrequent(nums: number[], k: number): number[] {
-  const freq = {}, sorted = []
+  const freq: Record<number, number> = {}
+  const count: Record<number, number[]> = []
+  const res: number[] = []
 
   for (const num of nums) freq[num] = (freq[num] || 0) + 1
 
-  for (const entry in freq) sorted.push([entry, freq[entry]])
 
-  sorted.sort((a, b) => b[1] - a[1])
+  for (const [key, val] of Object.entries(freq)) {
+    if (count[val]) count[val].push(+key)
+    else count[val] = [+key]
+  }
 
-  return sorted.slice(0, k).map(entry => entry[0])
+  for (let i = nums.length; i > 0 && res.length < k; i--) {
+    if (count[i]) res.push(...count[i]);
+    if (res.length >= k) break;
+  }
+
+  return res
 };
