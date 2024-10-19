17f027bc0222fb96ef6cb0e86ab07ba42011eacb
Hüsam <husamahmud@gmail.com>
2024-10-19 16:27:35 +0300

    [128] [Time Beats: 34.13%] [Memory Beats: 52.85%] - LeetPush

--- neetcode/128-Longest-Consecutive-Sequence.ts
+++ neetcode/128-Longest-Consecutive-Sequence.ts
@@ -1,45 +1,51 @@
-// 1: Brute force approach (Time Limit Exceeded)
-
+/* 1: Brute force approach (Time Limit Exceeded) */
 // function longestConsecutive(nums: number[]): number {
 //   if (nums.length === 0) return 0
-
 //   let maxStreak: number = 0
-
 //   for (const num of nums) {
 //     let curNum: number = num
 //     let curStreak: number = 1
-
 //     if (!nums.includes(curNum - 1)) {
 //       while (nums.includes(curNum + 1)) {
 //         curNum++, curStreak++
 //       }
-
 //       maxStreak = Math.max(maxStreak, curStreak)
 //     }
 //   }
-
 //   return maxStreak
 // }
 
-// 2: Sort the array first and search
+/* 2: Sort the array first and search */
+// function longestConsecutive(nums: number[]): number {
+//   if (nums.length === 0) return 0
+//   nums = nums.sort((a: number, b: number) => a - b)
+//   let maxStreak: number = 0
+//   let curStreak: number = 1
+//   for (let i = 1; i < nums.length; i++) {
+//     if (nums[i] === nums[i - 1]) continue
+//     if (nums[i] === nums[i - 1] + 1) {
+//       curStreak++
+//     } else {
+//       maxStreak = Math.max(maxStreak, curStreak)
+//       curStreak = 1
+//     }
+//   }
+//   return Math.max(maxStreak, curStreak)
+// }
+
+/* 3: use set for faster lookup */
 function longestConsecutive(nums: number[]): number {
-  if (nums.length === 0) return 0
+  const set: Set<number> = new Set(nums)
+  let max: number = 0
 
-  nums = nums.sort((a: number, b: number) => a - b)
+  for (const num of nums) {
+    if (!set.has(num - 1)) {
+      let cur: number = 0
 
-  let maxStreak: number = 0
-  let curStreak: number = 1
-
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] === nums[i - 1]) continue
-
-    if (nums[i] === nums[i - 1] + 1) {
-      curStreak++
-    } else {
-      maxStreak = Math.max(maxStreak, curStreak)
-      curStreak = 1
+      while (set.has(num + cur)) cur++
+      max = Math.max(max, cur)
     }
   }
 
-  return Math.max(maxStreak, curStreak)
+  return max
 }
