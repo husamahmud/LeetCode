faa987902ffd768ca455aee65b317e9634ba24cb
Hüsam <husamahmud@gmail.com>
2024-10-19 16:11:59 +0300

    [128] [Time Beats: 100.00%] [Memory Beats: 96.63%] - LeetPush

---
+++ neetcode/128-Longest-Consecutive-Sequence.ts
@@ -0,0 +1,45 @@
+// 1: Brute force approach (Time Limit Exceeded)
+
+// function longestConsecutive(nums: number[]): number {
+//   if (nums.length === 0) return 0
+
+//   let maxStreak: number = 0
+
+//   for (const num of nums) {
+//     let curNum: number = num
+//     let curStreak: number = 1
+
+//     if (!nums.includes(curNum - 1)) {
+//       while (nums.includes(curNum + 1)) {
+//         curNum++, curStreak++
+//       }
+
+//       maxStreak = Math.max(maxStreak, curStreak)
+//     }
+//   }
+
+//   return maxStreak
+// }
+
+// 2: Sort the array first and search
+function longestConsecutive(nums: number[]): number {
+  if (nums.length === 0) return 0
+
+  nums = nums.sort((a: number, b: number) => a - b)
+
+  let maxStreak: number = 0
+  let curStreak: number = 1
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) continue
+
+    if (nums[i] === nums[i - 1] + 1) {
+      curStreak++
+    } else {
+      maxStreak = Math.max(maxStreak, curStreak)
+      curStreak = 1
+    }
+  }
+
+  return Math.max(maxStreak, curStreak)
+}
