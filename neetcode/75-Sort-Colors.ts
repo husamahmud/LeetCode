0e14759ce696d2806801f991c0030669610f55b1
Hüsam <husamahmud@gmail.com>
2024-10-19 16:54:43 +0300

    [75] [Time Beats: 100.00%] [Memory Beats: 18.05%] - LeetPush

--- neetcode/75-Sort-Colors.ts
+++ neetcode/75-Sort-Colors.ts
@@ -2,15 +2,28 @@
  * Do not return anything, modify nums in-place instead.
  */
 /* 1: Bucket sort */
+// function sortColors(nums: number[]): void {
+//   const count: number[] = new Array(3).fill(0)
+//   let idx: number = 0
+//   for (const num of nums) count[num]++
+//   for (let i = 0; i < count[0]; i++) nums[idx++] = 0
+//   for (let i = 0; i < count[1]; i++) nums[idx++] = 1
+//   for (let i = 0; i < count[2]; i++) nums[idx++] = 2
+// }
+
+/* 2: Quick sort */
 function sortColors(nums: number[]): void {
-  const count: number[] = new Array(3).fill(0)
+  let l = 0, r = nums.length - 1, i = 0
 
-  // count each occurrence
-  for (const num of nums) count[num]++
+  while (i <= r) {
+    if (nums[i] === 0) {
+      [nums[i], nums[l]] = [nums[l], nums[i]]
+      l++
+    } else if (nums[i] === 2) {
+      [nums[i], nums[r]] = [nums[r], nums[i]]
+      r--, i--
+    }
 
-  let idx: number = 0
-
-  for (let i = 0; i < count[0]; i++) nums[idx++] = 0
-  for (let i = 0; i < count[1]; i++) nums[idx++] = 1
-  for (let i = 0; i < count[2]; i++) nums[idx++] = 2
+    i++
+  }
 }
