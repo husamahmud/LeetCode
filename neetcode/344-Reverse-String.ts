4d3dc12c4b9278455d7134f0515d8037ccb44335
Hüsam <husamahmud@gmail.com>
2024-10-22 19:29:55 +0300

    [344] [Time Beats: 97.65%] [Memory Beats: 20.72%] - LeetPush

---
+++ neetcode/344-Reverse-String.ts
@@ -0,0 +1,11 @@
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+  let l = 0, r = s.length - 1
+
+  while (l < r) {
+    [s[l], s[r]] = [s[r], s[l]]
+    l++, r--
+  }
+}; 
