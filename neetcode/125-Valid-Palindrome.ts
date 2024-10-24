55de8ec9e02d7dcc387d857616af49c1927dd09b
Hüsam <husamahmud@gmail.com>
2024-10-24 16:07:17 +0300

    [125] [Time Beats: 93.61%] [Memory Beats: 19.03%] - LeetPush

---
+++ neetcode/125-Valid-Palindrome.ts
@@ -0,0 +1,18 @@
+function isAlnum(c: string): boolean {
+  return !!c.match(/[a-zA-Z0-9]/)
+}
+
+function isPalindrome(s: string): boolean {
+  let l: number = 0, r: number = s.length - 1;
+
+  while (l < r) {
+    if (!isAlnum(s[l])) l++
+    else if (!isAlnum(s[r])) r--
+    else {
+      if (s[l].toLowerCase() !== s[r].toLowerCase()) return false
+      l++, r--
+    }
+  }
+
+  return true
+};
