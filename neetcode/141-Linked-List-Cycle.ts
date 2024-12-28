6ac8c9da6d9b6ff1b0e51c093c7a61d6097cb74b
Hüsam <husamahmud@gmail.com>
2024-12-28 14:54:34 +0300

    [141] [Time Beats: 93.87%] [Memory Beats: 47.74%] - LeetPush

---
+++ neetcode/141-Linked-List-Cycle.ts
@@ -0,0 +1,12 @@
+function hasCycle(head: ListNode | null): boolean {
+  let slow = head, fast = head
+
+  while (fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
+
+    if (slow === fast) return true
+  }
+
+  return false
+};
