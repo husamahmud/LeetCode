1ba130b115f5472594c19394199c3390f9c51e98
Hüsam <husamahmud@gmail.com>
2025-01-29 14:48:59 +0300

    [347] [Time Beats: 39.78%] [Memory Beats: 39.46%] - LeetPush

---
+++ pstrack/347-Top-K-Frequent-Elements.ts
@@ -0,0 +1,18 @@
+function topKFrequent(nums: number[], k: number): number[] {
+  const freq: Record<number, number> = {}
+  const count: number[][] = []
+  const res: number[] = []
+
+  for (const num of nums) freq[num] = (freq[num] || 0) + 1
+
+  for (const [key, val] of Object.entries(freq)) {
+    if (count[val]) count[val].push(+key)
+    else count[val] = [+key]
+  }
+
+  for (let i = nums.length; i > 0 && res.length < k; i--) {
+    if (count[i]) res.push(...count[i])
+  }
+
+  return res
+};
