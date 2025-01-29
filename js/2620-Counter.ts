d34d1006ffe2ad6fdc9bbeb461336649cf09ff85
Hüsam <husamahmud@gmail.com>
2025-01-29 15:47:35 +0300

    [2620] [Time Beats: 79.65%] [Memory Beats: 43.46%] - LeetPush

---
+++ js/2620-Counter.ts
@@ -0,0 +1,15 @@
+function createCounter(n: number): () => number {
+  let counter = n
+
+  return function () {
+    return counter++
+  }
+}
+
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
