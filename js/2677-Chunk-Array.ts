880e5a1d893731294f846b632e7142b8fbf8527d
Hüsam <husamahmud@gmail.com>
2025-02-02 13:15:54 +0300

    [2677] [Time Beats: 51.55%] [Memory Beats: 23.64%] - LeetPush

---
+++ js/2677-Chunk-Array.ts
@@ -0,0 +1,21 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | Array<JSONValue>;
+
+function chunk(arr: Obj[], size: number): Obj[] {
+  let count: number = size
+  let currChunk: Obj[] = []
+  let i: number
+  const res: Obj[][] = []
+
+  for (i = 0; i < arr.length; i++) {
+    currChunk.push(arr[i])
+
+    if (currChunk.length === size) {
+      res.push(currChunk)
+      currChunk = []
+    }
+  }
+
+  if (currChunk.length) res.push(currChunk)
+  return res
+};
