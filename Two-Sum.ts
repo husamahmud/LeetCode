17f4150455b08500f4352a6c92f5f69bdc643448
Hüsam <husamahmud@gmail.com>
2025-03-22 05:46:26 +0300

    [1] [Time Beats: 100.00%] [Memory Beats: 29.18%] - LeetPush

--- Two-Sum.ts
+++ Two-Sum.ts
@@ -1,10 +1,12 @@
 function twoSum(nums: number[], target: number): number[] {
-  const map = {}
+  const map: Record<number, number> = {}
 
   for (let i = 0; i < nums.length; i++) {
     const dif = target - nums[i]
 
-    if (dif in map) return [map[dif], i]
+    if (map[dif] !== undefined) return [map[dif], i]
     map[nums[i]] = i
   }
+
+  return [-1]
 };
