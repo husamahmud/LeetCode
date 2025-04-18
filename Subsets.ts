9e0fd340969d743c7467991d1403a7b693ec64ad
Hüsam <husamahmud@gmail.com>
2025-04-18 16:20:22 +0300

    [78] [Time Beats: 100.00%] [Memory Beats: 90.00%] - LeetPush

--- Subsets.ts
+++ Subsets.ts
@@ -1,19 +1,19 @@
 function subsets(nums: number[]): number[][] {
   const res: number[][] = []
-  const n: number = nums.length
 
-  function fn(i: number, subset: number[]): void {
-    if (i >= n) {
+  const rec = (i: number, subset: number[]): void => {
+    if (i === nums.length) {
       res.push([...subset])
       return
     }
 
     subset.push(nums[i])
-    fn(i + 1, subset)
+    rec(i + 1, subset)
+
     subset.pop()
-    fn(i + 1, subset)
+    rec(i + 1, subset)
   }
 
-  fn(0, [])
+  rec(0, [])
   return res
-}
+};
