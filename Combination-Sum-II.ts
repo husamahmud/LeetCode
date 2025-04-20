c85f5957c3ea9da98e19f26d92741e59dcffd386
Hüsam <husamahmud@gmail.com>
2025-04-20 16:18:04 +0300

    [40] [Time Beats: 72.21%] [Memory Beats: 32.38%] - LeetPush

--- Combination-Sum-II.ts
+++ Combination-Sum-II.ts
@@ -1,26 +1,24 @@
 function combinationSum2(candidates: number[], target: number): number[][] {
-  const res = new Set<number[]>()
+  const res: number[][] = []
   candidates.sort((a, b) => a - b)
 
-  const fn = (i: number, t: number, comb: number[]) => {
-    if (t === 0) {
-      res.add([...comb])
+  const rec = (i: number, t: number, ds: number[]): void => {
+    if (i === candidates.length) {
+      if (t === 0) res.push([...ds])
       return
     }
 
-    if (i >= candidates.length || t < 0) return
-
     if (candidates[i] <= t) {
-      comb.push(candidates[i])
-      fn(i + 1, t - candidates[i], comb)
-      comb.pop()
+      ds.push(candidates[i])
+      rec(i + 1, t - candidates[i], ds)
+      ds.pop()
     }
 
     while (i + 1 < candidates.length && candidates[i] === candidates[i + 1]) i++
 
-    fn(i + 1, t, comb)
+    rec(i + 1, t, ds)
   }
 
-  fn(0, target, [])
-  return Array.from(res)
+  rec(0, target, [])
+  return res
 };
