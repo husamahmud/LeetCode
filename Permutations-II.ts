e3cbead697c15f562ead472812020d3604c53435
Hüsam <husamahmud@gmail.com>
2025-04-23 20:50:36 +0300

    [47] [Time Beats: 54.91%] [Memory Beats: 64.73%] - LeetPush

--- Permutations-II.ts
+++ Permutations-II.ts
@@ -27,28 +27,3 @@ function permuteUnique(nums: number[]): number[][] {
   rec()
   return res
 }
-
-// const res: number[][] = []
-// nums.sort((a, b) => a - b)
-
-// const rec = (f: boolean[], ds: number[]): void => {
-//   if (ds.length === nums.length) {
-//     res.push([...ds])
-//     return
-//   }
-
-//   for (let i = 0; i < nums.length; i++) {
-//     if (f[i]) continue
-
-//     f[i] = true
-//     ds.push(nums[i])
-
-//     rec(f, ds)
-
-//     ds.pop()
-//     f[i] = false
-//   }
-// }
-
-// rec([], [])
-// return res
